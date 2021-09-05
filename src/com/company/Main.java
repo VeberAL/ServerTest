697667064597bee06df3b4712b1537d641ff493e
VeberAL <aleo83@rambler.ru>
2021-09-05 18:27:47 +0300

    HTTP single-threaded Server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -16,26 +18,29 @@ public class Main {
         new Server().bootstrap();
     }
 }
-
 class Server {
     private final static int BUFFER_SIZE = 256;
     private AsynchronousServerSocketChannel server;
+    private final static String HEADERS =
+            "HTTP/1.1 200 OK\n" +
+            "Server: test\n" +
+            "Content-Type: text/html\n" +
+            "Content-Length: %s\n" +
+            "Connection: close\n\n";
 
     public void bootstrap() {
         try {
             server = AsynchronousServerSocketChannel.open();
             server.bind(new InetSocketAddress("127.0.0.1", 8088));
             //соединение
-            Future <AsynchronousSocketChannel> future = server.accept();
-            System.out.println("New client");
-            AsynchronousSocketChannel clientChannel = future.get(30, TimeUnit.SECONDS);
-            //чтение
-            while (clientChannel != null && clientChannel.isOpen()) {
-                ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
-                clientChannel.read(buffer).get();
-                clientChannel.write(buffer).get();
-                clientChannel.close();
+
+            //цикл, в котором выполняются все запросы
+            while (true)
+            {
+                Future <AsynchronousSocketChannel> future = server.accept();
+                handleClient(future);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -44,6 +49,38 @@ class Server {
             e.printStackTrace();
         } catch (TimeoutException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void handleClient(Future<AsynchronousSocketChannel> future)
+         throws InterruptedException, ExecutionException, TimeoutException, IOException {
+        System.out.println("New client");
+        AsynchronousSocketChannel clientChannel = future.get(30, TimeUnit.SECONDS);
+        //чтение
+        while (clientChannel != null && clientChannel.isOpen()) {
+            ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+            StringBuilder builder = new StringBuilder();
+            boolean keepReading = true;
+            while (keepReading) {
+                clientChannel.read(buffer).get();
+
+                int position = buffer.position();
+                keepReading = position == BUFFER_SIZE;
+
+                byte[] array = keepReading
+                        ? buffer.array()
+                        : Arrays.copyOfRange(buffer.array(), 0, position);
+
+
+                builder.append(new String(buffer.array()));
+                buffer.clear();
+            }
+            String body = "<html><body><h1>Hello,test</h1></body></html>";
+            String page = String.format(HEADERS, body.length()) + body;
+
+            ByteBuffer resp = ByteBuffer.wrap(page.getBytes());
+            clientChannel.write(resp);
+            clientChannel.close();
         }
     }
 }
